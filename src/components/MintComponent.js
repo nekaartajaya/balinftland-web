133ee5d6f9f8c586c16f28906b6ea31892783c80
ical10 <mochhusnir@gmail.com>
2022-05-11 11:47:58 +0700

    implemented addWalletListener to listen for wallet state changes

--- src/components/MintComponent.js
+++ src/components/MintComponent.js
@@ -19,6 +19,31 @@ const MintComponent = () => {
   //Constant state
   const [maxQuantity] = useState(80);
 
+  const addWalletListener = () => {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  };
+
   const fetchCurrentWallet = async () => {
     const { address, status } = await getCurrentWalletConnected();
     return { address, status };
@@ -28,6 +53,8 @@ const MintComponent = () => {
     const { address, status } = fetchCurrentWallet();
     setWallet(address);
     setStatus(status);
+
+    addWalletListener();
   }, []);
 
   const handleConnect = async () => {
