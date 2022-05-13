8e7b635cd6d91ffc4f16c6f8035c18df44354573
ical10 <mochhusnir@gmail.com>
2022-05-13 15:21:33 +0700

    created mintDigilandNFT function

--- src/helpers/metamask-interact.js
+++ src/helpers/metamask-interact.js
@@ -1,3 +1,6 @@
+import { createAlchemyWeb3 } from "@alch/alchemy-web3";
+const contractABI = require("../../contracts/LBSFragment.json");
+
 export const connectWallet = async () => {
   if (window.ethereum) {
     try {
@@ -50,6 +53,51 @@ export const getCurrentWalletConnected = async () => {
     return {
       address: "",
       status: "please install metamask",
+    };
+  }
+};
+
+export const mintDigilandNFT = async (quantity) => {
+  const alchemyKey = process.env.NEXT_PUBLIC_REACT_APP_ALCHEMY_KEY;
+  console.log({ alchemyKey });
+  const web3 = createAlchemyWeb3(alchemyKey);
+
+  const contractAddress = "0x3a0508AF8eCDE61C99083Fbb263d6a99BFe05eCe";
+
+  const tokenId = 4; // Active stage
+  const data = "0x00";
+
+  //load smart contract
+  window.contract = await new web3.eth.Contract(
+    contractABI.abi,
+    contractAddress
+  ); //loadContract();
+
+  //set up your Ethereum transaction
+  const transactionParameters = {
+    to: contractAddress, // Required except during contract publications.
+    from: window.ethereum.selectedAddress, // must match user's active address.
+    data: window.contract.methods.mint(tokenId, quantity, data).encodeABI(), //make call to NFT smart contract
+  };
+
+  console.log({ tokenId });
+
+  //sign transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: "eth_sendTransaction",
+      params: [transactionParameters],
+    });
+    return {
+      success: true,
+      status:
+        "✅ Check out your transaction on Etherscan: https://rinkeby.etherscan.io/tx/" +
+        txHash,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message,
     };
   }
 };
