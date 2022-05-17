4cf42cb1fe6e1a1769a738df38ff4ed3fbe84752
ical10 <mochhusnir@gmail.com>
2022-05-17 11:45:19 +0700

    init styles on Navbar

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,42 +1,30 @@
+import Link from "next/link";
+
 const NavbarComponent = ({ onConnect, walletAddress }) => {
   return (
-    <div id="navbar">
-      <div style={{ marginLeft: 20 }}>
-        <div>
-          <h1>
-            <img src="/ProperBlockLogo.svg" alt="" />
-            PROPERBLOCK
-          </h1>
-        </div>
+    <nav id="navbar">
+      <div className="logo">
+        <img src="/ProperBlockLogo.svg" alt="ProperBlock-logo" />
+        <h1>PROPERBLOCK</h1>
       </div>
 
-      <div style={{ paddingRight: 20 }}>
-        <div>
-          <h1>whitepaper</h1>
-        </div>
+      <Link href="">whitepaper</Link>
+      <Link href="">how it works</Link>
+      <Link href="">projects</Link>
 
-        <div>
-          <h1>how it works</h1>
-        </div>
-
-        <div>
-          <h1>projects</h1>
-        </div>
-
-        <div>
-          <button onClick={onConnect}>
-            {walletAddress && walletAddress.length > 0 ? (
-              "Connected: " +
-              String(walletAddress).substring(0, 6) +
-              "..." +
-              String(walletAddress).substring(38)
-            ) : (
-              <span>Connect Wallet</span>
-            )}
-          </button>
-        </div>
+      <div>
+        <button onClick={onConnect}>
+          {walletAddress && walletAddress.length > 0 ? (
+            "Connected: " +
+            String(walletAddress).substring(0, 6) +
+            "..." +
+            String(walletAddress).substring(38)
+          ) : (
+            <span>Connect Wallet</span>
+          )}
+        </button>
       </div>
-    </div>
+    </nav>
   );
 };
 
