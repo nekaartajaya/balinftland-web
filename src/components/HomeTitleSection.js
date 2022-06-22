784ce79c097abec4a93e4a85b8bf15e0506d19e7
nekaartajaya <nekaartajaya@gmail.com>
2022-06-22 22:03:03 +0800

    feat : create animation for content

--- src/components/HomeTitleSection.js
+++ src/components/HomeTitleSection.js
@@ -1,5 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
+import {useRef} from 'react';
+import {useSpring, animated, easings} from 'react-spring';
+
 import styles from '../../styles/HomeTitleSection.module.css';
+import useIntersectionObserver from '../hooks/useIntersectionObserver';
 
 const HomeTitleSection = ({
   type,
@@ -11,45 +15,81 @@ const HomeTitleSection = ({
   topRightText,
   children,
 }) => {
+  const triggerRef = useRef();
+  const dataRef = useIntersectionObserver(triggerRef, {
+    freezeOnceVisible: true,
+  });
+  const fadeIn = useSpring({
+    from: {opacity: 0},
+    to: {opacity: dataRef?.isIntersecting ? 1 : 0},
+    delay: 100,
+    config: {
+      duration: 1200,
+    },
+  });
+  const borderWidth = useSpring({
+    from: {width: '0%'},
+    to: {width: dataRef?.isIntersecting ? '100%' : '0%'},
+    delay: 100,
+    config: {
+      duration: 2000,
+      easing: easings.easeOutQuint,
+    },
+  });
+  const fadeInUp = useSpring({
+    from: {transform: 'translateY(100%)'},
+    to: {transform: dataRef?.isIntersecting ? 'translateY(0%)' : 'translateY(100%)'},
+    delay: 300,
+    config: {
+      duration: 800,
+    },
+  });
+
   return (
     <div className={styles.container}>
-      <div className={styles.top}>
-        <div className={styles.left}>{topLeftText}</div>
-        <div className={styles.right}>
-          {topRightText}
-          <span className={styles.dot}>.</span>
-        </div>
+      <div className="overflow-hidden">
+        <animated.div style={fadeInUp} className={styles.top} ref={triggerRef}>
+          <div className={styles.left}>{topLeftText}</div>
+          <div className={styles.right}>
+            {topRightText}
+            <span className={styles.dot}>.</span>
+          </div>
+        </animated.div>
       </div>
-      <div className={styles.border}></div>
-      {type === 'leftImage' ? (
-        <div
-          className={`${styles.bottomLeftImage} ${styles.bottom}`}
-          style={{alignItems: imageURL ? 'center' : 'flex-start'}}
-        >
-          <div className={styles.left}>
-            {imageURL ? (
-              <div className={styles.imageContainer}>
-                <img src={imageURL} className={styles.image} alt={imageAlt} />
-              </div>
-            ) : null}
-            <div className={styles.miniText}>{miniText}</div>
+      <div className="w-full inline-block">
+        <animated.div style={borderWidth} className={styles.border} ref={triggerRef}></animated.div>
+      </div>
+      <animated.div style={fadeIn} ref={triggerRef}>
+        {type === 'leftImage' ? (
+          <div
+            className={`${styles.bottomLeftImage} ${styles.bottom}`}
+            style={{alignItems: imageURL ? 'center' : 'flex-start'}}
+          >
+            <div className={styles.left}>
+              {imageURL ? (
+                <div className={styles.imageContainer}>
+                  <img src={imageURL} className={styles.image} alt={imageAlt} />
+                </div>
+              ) : null}
+              <div className={styles.miniText}>{miniText}</div>
+            </div>
+            <div className={styles.right}>{descText}</div>
           </div>
-          <div className={styles.right}>{descText}</div>
-        </div>
-      ) : type === 'topImage' ? (
-        <div className={`${styles.bottomTopImage} ${styles.bottom}`}>
-          <div className={styles.left}>
-            {imageURL ? (
-              <div className={styles.imageContainer}>
-                <img src={imageURL} className={styles.image} alt={imageAlt} />
-              </div>
-            ) : null}
+        ) : type === 'topImage' ? (
+          <div className={`${styles.bottomTopImage} ${styles.bottom}`}>
+            <div className={styles.left}>
+              {imageURL ? (
+                <div className={styles.imageContainer}>
+                  <img src={imageURL} className={styles.image} alt={imageAlt} />
+                </div>
+              ) : null}
+            </div>
+            <div className={styles.right}>{descText}</div>
           </div>
-          <div className={styles.right}>{descText}</div>
-        </div>
-      ) : type === 'custom' ? (
-        children
-      ) : null}
+        ) : type === 'custom' ? (
+          children
+        ) : null}
+      </animated.div>
     </div>
   );
 };
--- src/components/ImageDescComponent.js
+++ src/components/ImageDescComponent.js
@@ -1,27 +1,61 @@
 /* eslint-disable @next/next/no-img-element */
+import {useRef} from 'react';
+import {useSpring, animated, easings} from 'react-spring';
+
 import styles from '../../styles/ImageDescComponent.module.css';
+import useIntersectionObserver from '../hooks/useIntersectionObserver';
 
 const ImageDescComponent = ({imageUrl, imageAlt, title, description}) => {
+  const triggerRef = useRef();
+  const dataRef = useIntersectionObserver(triggerRef, {
+    freezeOnceVisible: true,
+  });
+
+  const fadeIn = useSpring({
+    from: {opacity: 0},
+    to: {opacity: dataRef?.isIntersecting ? 1 : 0},
+    delay: 100,
+    config: {
+      duration: 1200,
+    },
+  });
+  const fadeInUp = useSpring({
+    from: {transform: 'translateY(120%)'},
+    to: {transform: dataRef?.isIntersecting ? 'translateY(0%)' : 'translateY(120%)'},
+    delay: 300,
+    config: {
+      duration: 800,
+    },
+  });
+
   return (
     <div className={styles.container}>
       {title ? (
-        <div className={`${styles.title} ${styles.top}`}>
+        <animated.div ref={triggerRef} style={fadeInUp} className={`${styles.title} ${styles.top}`}>
           {title}
           <span className={styles.dot}>.</span>
-        </div>
+        </animated.div>
       ) : null}
       <div className="flex">
-        <div className={styles.imageContainer}>
+        <animated.div ref={triggerRef} style={fadeIn} className={styles.imageContainer}>
           <img src={imageUrl} className={styles.image} alt={imageAlt} />
-        </div>
+        </animated.div>
         <div className={styles.textContainer}>
           {title ? (
-            <div className={`${styles.title} ${styles.bottom}`}>
-              {title}
-              <span className={styles.dot}>.</span>
+            <div className="overflow-hidden">
+              <animated.div
+                ref={triggerRef}
+                style={fadeInUp}
+                className={`${styles.title} ${styles.bottom}`}
+              >
+                {title}
+                <span className={styles.dot}>.</span>
+              </animated.div>
             </div>
           ) : null}
-          <div className={styles.description}>{description}</div>
+          <animated.div ref={triggerRef} style={fadeIn} className={styles.description}>
+            {description}
+          </animated.div>
         </div>
       </div>
     </div>
