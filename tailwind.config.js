7f7141dce37c6a129830b308e4166f3a3b155915
nekaartajaya <nekaartajaya@gmail.com>
2022-11-04 12:55:02 +0800

    feat : setup colors tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      'dark-blue': '#0E3D5B',
+      'dark-blue-2': '#231E58',
+      blue: '#155E9D',
+      'light-blue': '#0175BF',
+      grey: '#838383',
+      'light-green': '#2AC610',
+      green: '#53BA9C',
+    },
     extend: {},
   },
   plugins: [],
