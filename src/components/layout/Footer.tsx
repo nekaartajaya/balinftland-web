0a9ab3fa829aca1299c7fb7a652ac52eb5df9fa0
nekaartajaya <nekaartajaya@gmail.com>
2022-11-07 15:29:54 +0800

    feat : footer mobile

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -82,14 +82,16 @@ const Footer = () => {
   ];
 
   return (
-    <div className="flex md:px-[60px] px-4">
-      <div className="w-1/3">
-        <div className="flex flex-col">
+    <div className="flex md:flex-row flex-col md:px-[60px] px-4 gap-y-4">
+      <div className="md:w-1/3 w-full">
+        <div className="flex md:flex-col justify-between">
           <div className="mb-4">
             <img src="/images/logo/logo-color.png" alt="BALINFTLAND-logo" className="" />
           </div>
           <div>
-            <div className="text-[13px] text-dark-blue-2 tracking-[4px] mb-2">FOLLOW US ON</div>
+            <div className="sm:text-[13px] text-[11px] text-dark-blue-2 tracking-[4px] mb-2">
+              FOLLOW US ON
+            </div>
             <div className="flex gap-x-4">
               {SocialMedia.map(({name, icon, link}) => {
                 return (
@@ -104,10 +106,10 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="w-2/3 flex">
+      <div className="md:w-2/3 w-full flex sm:flex-row flex-col gap-y-4">
         {footerMenu.map(({title, list}: FooterMenu) => {
           return (
-            <div className="w-1/3 text-sm tracking-wide">
+            <div className="sm:w-1/3 text-sm tracking-wide">
               <div className="uppercase text-dark-blue-2 font-bold mb-3">{title}</div>
               {list.map(({name, link}: ListFooterMenu) => {
                 return <div className="capitalize text-grey font-light mb-3">{name}</div>;
