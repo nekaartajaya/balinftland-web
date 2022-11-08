383ee27a73ea7cdeed708f54757341d3806f319b
nekaartajaya <nekaartajaya@gmail.com>
2022-11-08 13:07:09 +0800

    feat : project top section

---
+++ src/components/project/TopSection.tsx
@@ -0,0 +1,26 @@
+const ProjectTopSection = () => {
+  return (
+    <>
+      <div className="max-w-[460px] w-full mx-auto">
+        <h1 className="text-[64px] tracking-wider leading-[80px] text-blue text-center mb-8">
+          <span className="font-bold">LIMA</span> BEACH SIGNATURE NFT.
+        </h1>
+      </div>
+
+      <div className="mb-10">
+        <img
+          src="/images/pages/limabeach/dummy-1.png"
+          alt="Introduce Lima Beach"
+          className="w-full"
+        />
+      </div>
+
+      <div className="text-blue text-[32px]">
+        A limited-edition NFT representing the physical property certificate and physical property
+        ownership built with the Internet of Things (IoT) Technology and Web 3.0.
+      </div>
+    </>
+  );
+};
+
+export default ProjectTopSection;
--- src/pages/project/lima-beach-signature.tsx
+++ src/pages/project/lima-beach-signature.tsx
@@ -1,9 +1,11 @@
 import Layout from '@components/layout';
 import ProjectStageSection from '@components/project/StageSection';
+import ProjectTopSection from '@components/project/TopSection';
 
 const LimaBeachSiganture = () => {
   return (
     <Layout>
+      <ProjectTopSection />
       <ProjectStageSection />
     </Layout>
   );
