c837a8e69369ef4da52f5c7486a36fc3718419e2
nekaartajaya <nekaartajaya@gmail.com>
2022-11-09 17:18:27 +0800

    feat : profile utility section

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,11 +1,13 @@
 import Layout from '@components/layout';
 import ProfileTopSection from '@components/profile/TopSection';
+import ProfileUtilitySection from '@components/profile/UtilitySection';
 
 const Profile = () => {
   return (
     <Layout>
       <div>
         <ProfileTopSection />
+        <ProfileUtilitySection />
       </div>
     </Layout>
   );
