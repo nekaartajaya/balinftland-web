82d53d20fbb7d3db5f301509ce5cee90e947b53c
nekaartajaya <nekaartajaya@gmail.com>
2022-11-15 17:24:43 +0800

    feat : home hospitality responsive

--- src/components/home/HospitalitySection.tsx
+++ src/components/home/HospitalitySection.tsx
@@ -1,6 +1,10 @@
 import {Hospitality} from '@interfaces/HomeInterface';
+import {useMediaQuery} from '@mui/material';
+import Carousel from 'react-multi-carousel';
 
 const HomeHospitalitySection = () => {
+  const isLaptop = useMediaQuery('(max-width: 1440px)', {noSsr: true});
+
   const Hospitality: Array<Hospitality> = [
     {
       name: 'Dummy',
@@ -34,20 +38,52 @@ const HomeHospitalitySection = () => {
     },
   ];
 
+  const responsive = {
+    desktop: {
+      breakpoint: {max: 9999, min: 991},
+      items: 6,
+    },
+    laptop: {
+      breakpoint: {max: 990, min: 769},
+      items: 4,
+      slidesToSlide: 1,
+    },
+    tablet: {
+      breakpoint: {max: 768, min: 401},
+      items: 2,
+      slidesToSlide: 1,
+    },
+    phone: {
+      breakpoint: {max: 400, min: 0},
+      items: 1,
+      slidesToSlide: 1,
+    },
+  };
+
   return (
-    <div className="py-24">
-      <h1 className="text-[52px] text-blue font-bold text-center mb-16">Smart 4.0 Hospitality</h1>
-      <div className="grid grid-cols-6 gap-x-6 px-4">
-        {Hospitality.map(({name, desc, image}, index) => {
-          return (
-            <div key={index}>
-              <div className="mb-4">
-                <img src={image} alt={name} className="w-full" />
+    <div className="md:py-24 py-16 md:px-0 px-4">
+      <h1 className="md:text-[52px] text-4xl text-blue font-bold text-center mb-16">
+        Smart 4.0 Hospitality
+      </h1>
+      <div className="px-4 relative pb-10">
+        <Carousel
+          responsive={responsive}
+          arrows={false}
+          showDots={isLaptop ? true : false}
+          renderDotsOutside={true}
+          dotListClass={'slider-dot'}
+        >
+          {Hospitality.map(({name, desc, image}, index) => {
+            return (
+              <div key={index} className="px-2">
+                <div className="mb-4">
+                  <img src={image} alt={name} className="w-full" />
+                </div>
+                <div className="text-center text-blue text-sm font-normal px-2">{desc}</div>
               </div>
-              <div className="text-center text-blue text-sm font-normal px-2">{desc}</div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </Carousel>
       </div>
     </div>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@styles/global.css';
 import '@styles/navbar.css';
+import 'react-multi-carousel/lib/styles.css';
 import type {AppProps} from 'next/app';
 
 export default function MyApp({Component, pageProps}: AppProps) {
