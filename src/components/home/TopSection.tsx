f1cecaeea8980a939ebf231e020fefdb35ae85f7
nekaartajaya <nekaartajaya@gmail.com>
2022-11-15 14:56:50 +0800

    feat : home top section responsive

--- src/components/home/TopSection.tsx
+++ src/components/home/TopSection.tsx
@@ -5,15 +5,15 @@ const HomeTopSection = () => {
   const router = useRouter();
 
   return (
-    <div className="flex justify-between items-center min-h-[772px] px-4">
-      <div className="w-1/2 px-24">
+    <div className="md:flex justify-between items-center lg:min-h-[772px] md:min-h-[500px] px-4 py-10">
+      <div className="lg:w-1/2 md:w-[60%] lg:px-24 px-10 mb-10">
         <h1 className="text-xl text-blue tracking-[8px] mb-4">BALI NFT LAND</h1>
-        <div className="text-7xl font-bold tracking-wider leading-tight text-blue mb-8">
+        <div className="lg:text-7xl text-5xl font-bold tracking-wider leading-tight text-blue mb-8">
           <h1>NFT,</h1>
           <h1>Blockchain,</h1>
           <h1>Property,</h1>
         </div>
-        <div>
+        <div className="md:block hidden">
           <CustomButton
             text="About Bali NFT Land"
             classes="text-base"
@@ -22,8 +22,15 @@ const HomeTopSection = () => {
         </div>
       </div>
 
-      <div className="w-1/2">
-        <img src="images/stages/stage-4.png" alt="Home Top Section" />
+      <div className="lg:w-1/2 md:w-[40%]">
+        <img src="images/stages/stage-4.png" alt="Home Top Section" className="w-full" />
+      </div>
+      <div className="md:hidden block mt-10">
+        <CustomButton
+          text="About Bali NFT Land"
+          classes="text-base mx-auto"
+          onClick={() => router.push('/profile')}
+        />
       </div>
     </div>
   );
