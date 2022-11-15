449d3c2676da6d94d3a8a3091e7c3199189b5358
nekaartajaya <nekaartajaya@gmail.com>
2022-11-15 15:20:32 +0800

    feat : home logo section responsive

--- src/components/home/LogoSection.tsx
+++ src/components/home/LogoSection.tsx
@@ -1,7 +1,11 @@
 import {Logo} from '@interfaces/HomeInterface';
-import {Avatar} from '@mui/material';
+import {Avatar, useMediaQuery} from '@mui/material';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 const HomeLogoSection = () => {
+  const isTab = useMediaQuery('(max-width: 768px)', {noSsr: true});
+
   const Logo: Array<Logo> = [
     {
       name: 'Logo',
@@ -24,17 +28,48 @@ const HomeLogoSection = () => {
       logo: '',
     },
   ];
+
+  const responsive = {
+    desktop: {
+      breakpoint: {max: 9999, min: 769},
+      items: 4,
+    },
+    tablet: {
+      breakpoint: {max: 768, min: 401},
+      items: 2,
+      slidesToSlide: 2,
+    },
+    phone: {
+      breakpoint: {max: 400, min: 0},
+      items: 1,
+      slidesToSlide: 1,
+    },
+  };
+
   return (
-    <div className="flex justify-around items-center bg-[url('/images/pages/home/dummy-11.png')] px-12 py-16">
-      {Logo.map(({name, desc}, index) => {
-        return (
-          <div key={index} className="flex flex-col items-center text-center gap-y-4 text-white">
-            <Avatar sx={{width: 100, height: 100}}> </Avatar>
-            <h1 className="text-sm">{name}</h1>
-            <div className="text-[10px] max-w-[180px]">{desc}</div>
-          </div>
-        );
-      })}
+    <div className="bg-[url('/images/pages/home/dummy-11.png')] bg-norepeat px-12 py-16">
+      <div className="relative pb-[50px]">
+        <Carousel
+          responsive={responsive}
+          arrows={false}
+          showDots={isTab ? true : false}
+          renderDotsOutside={true}
+          dotListClass={'slider-dot'}
+        >
+          {Logo.map(({name, desc}, index) => {
+            return (
+              <div
+                key={index}
+                className="flex flex-col items-center text-center gap-y-4 text-white"
+              >
+                <Avatar sx={{width: 100, height: 100}}> </Avatar>
+                <h1 className="text-sm">{name}</h1>
+                <div className="text-[10px] max-w-[180px]">{desc}</div>
+              </div>
+            );
+          })}
+        </Carousel>
+      </div>
     </div>
   );
 };
