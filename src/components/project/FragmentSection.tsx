0444c96d1e429817de0b7ddfbc6d918b250e8944
nekaartajaya <nekaartajaya@gmail.com>
2022-11-21 14:36:44 +0800

    feat: project stage section responsive

--- src/components/project/FragmentSection.tsx
+++ src/components/project/FragmentSection.tsx
@@ -1,18 +1,18 @@
 const ProjectFragmentSection = () => {
   return (
-    <div>
-      <div className="mb-4">
-        <h1 className="text-[64px] leading-[80px] text-blue text-left fo">
+    <div className="md:px-24 px-10">
+      <div className="mb-10">
+        <h1 className="md:text-[64px] text-4xl md:leading-[80px] leading-[50px] text-blue text-center">
           <span className="font-bold">LIMA</span> BEACH
           <br />
           NFT FRAGMENTS
         </h1>
       </div>
-      <div className="text-blue text-[32px] italic mb-4">
+      <div className="text-blue md:text-[32px] text-2xl italic mb-4 font-semibold text-justify">
         7 NFT fragments worth 1 luxury service apartment with a 58-years
         leasehold
       </div>
-      <div className="text-blue text-[22px] italic mb-4">
+      <div className="text-blue md:text-[22px] text-lg italic mb-4 text-justify">
         Each fragment will let the NFT holders stay in the apartment for up to
         30 nights. The quotas will be renewes every year for the next 58 years
         on 31 December 23:59:59 (UTC+8).
