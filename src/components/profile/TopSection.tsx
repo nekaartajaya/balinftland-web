a499ee223e6ad9fec1e869d576f74eb7f3584d55
nekaartajaya <nekaartajaya@gmail.com>
2022-12-07 11:58:12 +0800

    feat: update profile top section

--- src/components/profile/TopSection.tsx
+++ src/components/profile/TopSection.tsx
@@ -1,22 +1,28 @@
+import Image from 'next/legacy/image';
+
 const ProfileTopSection = () => {
   return (
     <>
       <div className="relative">
-        <img
-          src="/images/pages/profile/dummy-1.png"
-          alt="Bali NFT Land"
-          className="w-full"
-        />
+        <div className="relative min-h-[430px] md:min-h-[500px] lg:min-h-[740px]">
+          <Image
+            src="/images/pages/profile/dummy-1.png"
+            alt="Bali NFT Land"
+            layout="fill"
+            objectFit="cover"
+            className="w-full"
+          />
+        </div>
         <div className="absolute top-0 left-0 w-full h-full bg-black/[.20]"></div>
         <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-full">
-          <h1 className="text-center md:text-[72px] text-4xl md:leading-[100px] text-white">
+          <h1 className="text-center md:text-[72px] sm:text-4xl text-[27px] md:leading-[100px] text-white">
             <span className="font-bold">Bali NFT Land,</span>
             <br />
             NFT Property Developer.
           </h1>
         </div>
       </div>
-      <div className="italic text-center text-blue md:text-[32px] text-xl sm:px-24 px-10 py-14 md:leading-[45px]">
+      <div className="italic text-left text-blue md:text-[32px] text-base sm:px-24 px-10 py-8 lg:py-14 md:leading-[45px]">
         <span className="font-bold">Bali NFT Land</span> is a decentralized
         property developer with no middleman and no high conventional marketing
         cost. The idea is to create a viable option for the people who are
